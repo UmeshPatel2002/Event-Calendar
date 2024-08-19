725371f21bc37b4e81e7727959f337d1cc1b8fa9
UmeshPatel2002 <bestumeshverma1@gmail.com>
2024-08-19 13:54:44 +0530

    removed hard coded events

--- src/context/EventContext.js
+++ src/context/EventContext.js
@@ -7,20 +7,7 @@ export const EventProvider = ({ children }) => {
   // Retrieve events from local storage or initialize with default events
   const loadEvents = () => {
     const savedEvents = localStorage.getItem('events');
-    return savedEvents ? JSON.parse(savedEvents) : [
-      {
-        id: 1,
-        title: 'Meeting',
-        start: new Date(2024, 7, 17, 10, 0), // Aug 17, 2024, 10:00 AM
-        end: new Date(2024, 7, 17, 12, 0), // Aug 17, 2024, 12:00 PM
-      },
-      {
-        id: 2,
-        title: 'Lunch',
-        start: new Date(2024, 8, 18, 13, 0), // Aug 18, 2024, 1:00 PM
-        end: new Date(2024, 9, 18, 14, 0), // Aug 18, 2024, 2:00 PM
-      },
-    ];
+    return savedEvents ? JSON.parse(savedEvents) : [ ];
   };
 
   const [events, setEvents] = useState(loadEvents);
